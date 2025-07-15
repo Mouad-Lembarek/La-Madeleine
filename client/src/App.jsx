4994d592c036dfee457fe37220fa523f8b2745ea
ikram <ikramobousfiha@gmail.com>
2025-07-15 16:50:09 +0800

    site web

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="home-background">
+      <div className="logo-container">
+        <img src="/La%20Madeleine.png" alt="La Madeleine Logo" className="main-logo" />
+      </div>
+      <div className="welcome-text">
+        <h1>Welcome to La Madeleine</h1>
+        <p>Experience the taste of tradition and elegance.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
